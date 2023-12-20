73771ec950a8acb996ba0fc5ed4a81f2e352b8be
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-12-20 13:38:31 +0300

    Обновление, удаление и получение

--- wa-server/app/exercise/exercise.routes.js
+++ wa-server/app/exercise/exercise.routes.js
@@ -2,10 +2,19 @@ import express from 'express'
 
 import { protect } from '../middleware/auth.middleware.js'
 
-import { createNewExercise, getExercises } from './exercise.controller.js'
+import { 
+    createNewExercise, 
+    getExercises, 
+    updateExercise, 
+    deleteExercise 
+} from './exercise.controller.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, createNewExercise).get(protect, getExercises)
+router
+	.route('/:id')
+	.put(protect, updateExercise)
+	.delete(protect, deleteExercise)
 
 export default router
